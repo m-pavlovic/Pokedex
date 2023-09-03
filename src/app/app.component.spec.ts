cfea4dba038f7fd98c8c0bbfc4de95a5bfcf0cd8
m-pavlovic <marija.pavlovic19@gmail.com>
2023-09-03 18:28:48 +0200

    Završni projekt - angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [
+      AppComponent,
+      PokemonListComponent]
   }));
 
   it('should create the app', () => {
